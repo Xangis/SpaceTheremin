26da566c4e0f517a4dbb4b37b3c4dcdd7e934f4a
Jason Champion <jchampion@zetacentauri.com>
2016-10-27 21:30:33 -0700

    Change library path settings for OSX.

--- installer/OSXLibraryPaths.sh
+++ installer/OSXLibraryPaths.sh
@@ -9,19 +9,19 @@ install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.dylib @executable_p
 install_name_tool -change /usr/local/lib/libwx_baseu_net-3.1.dylib @executable_path/../Frameworks/libwx_baseu_net-3.1.dylib SpaceTheremin.app/Contents/MacOS/SpaceTheremin
 install_name_tool -change /usr/local/lib/libwx_baseu-3.1.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/MacOS/SpaceTheremin
 # wxWidget library references to other wxWidgets libraries.
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_core-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_baseu_net-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu_xml-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_baseu_xml-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_baseu_xml-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_html-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_osx_cocoau_html-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_adv-3.1.0.0.0.dylib @executable_path../Frameworks/libwx_osx_cocoau_adv-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_core-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_baseu_net-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu_xml-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_baseu_xml-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu_xml-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_html-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_html-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_adv-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_adv-3.1.dylib SpaceTheremin.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
 otool -L SpaceTheremin.app/Contents/MacOS/SpaceTheremin
